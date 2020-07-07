80d847e599d1871436178f4511941f7adc6d1497
Xhanty <38590378+Xhanty@users.noreply.github.com>
2020-07-07 10:59:08 -0500

    Conexión Base De Datos

--- app/Http/Controllers/FirebaseController.php
+++ app/Http/Controllers/FirebaseController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\ServiceAccount;
 
 class FirebaseController extends Controller
 {
-    //
+    public function index(){
+        $serviceAccount = ServiceAccount::fromJsonFile(__DIR__. '/FirebaseKey.json');
+
+        $firebase = (new Factory)
+        ->withServiceAccount($serviceAccount)
+        ->create(); 
+
+        $database = $firebase->getDatabase();
+
+        $ref = $database->getReference('Usuarios');
+
+        $key = $ref->push()->getKey();
+
+        return $key;
+    }
 }
